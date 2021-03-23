2b6cb79dc86a781e58fbe010ee6b0d848e4ab731
Christian Gomez Martinez <gomez9289@gmail.com>
2021-03-23 18:14:54 -0400

    refactoring

--- script.js
+++ script.js
@@ -8,34 +8,34 @@ var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialCharacters = ["!", "@", "#", "$", "%", "&", "*", "<", ">", "-", "+"];
 var allCharacters = [];
 
-while (length <= 8 || length >= 128) {
-  var length = prompt(
-    "How many characters would you like your password to be?"
-  );
-  if (length <= 8) {
-    alert("Password has too be at least 8 and no more than 128 characters");
-  } else if (length >= 128) {
-    alert("Password has too be at least 8 and no more than 128 characters");
-  }
-}
+// while (length <= 8 || length >= 128) {
+//   var length = prompt(
+//     "How many characters would you like your password to be?"
+//   );
+//   if (length <= 8) {
+//     alert("Password has too be at least 8 and no more than 128 characters");
+//   } else if (length >= 128) {
+//     alert("Password has too be at least 8 and no more than 128 characters");
+//   }
+// }
 
 var confirmLowerCase = confirm("Would you like to use lower case letters?");
 if (confirmLowerCase === true) {
-  var allCharacters = lowercase.concat(allCharacters);}
+  allCharacters = lowercase.concat(allCharacters);}
 
 var confirmUpperccase = confirm("Would you like to use uppercase letters");
 if (confirmUpperccase === true)
-  var allCharacters = uppercase.concat(allCharacters);
+  allCharacters = uppercase.concat(allCharacters);
 
 var confirmNumeric = confirm("Would you like to use numeric characters?");
 if (confirmUpperccase === true)
-  var allCharacters = numeric.concat(allCharacters);
+  allCharacters = numeric.concat(allCharacters);
 
 var confirmSpecialCharacters = confirm(
   "Would you like to use special characters?"
 );
 if (confirmUpperccase === true)
-  var allCharacters = specialCharacters.concat(allCharacters);
+  allCharacters = specialCharacters.concat(allCharacters);
 
 // Write password to the #password input
 function writePassword() {
@@ -43,20 +43,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  // function generatePassword() {
-  //   for (var i = 0; i < 127; ) {
-  //     var newLetter = [j];
-  //     var j = Math.floor(Math.random() * length);
-
-  //     password = password + newLetter;
-  //   }
-  // }
 }
 writePassword()
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
 
-// function writePassword() {
-//   alert("your password is" + password);
-// }
